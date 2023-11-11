2b57049b53aecec8e3f567e2e7bbdb3be41ae03b
MFarrelAkbar1 <142593668+MFarrelAkbar1@users.noreply.github.com>
2023-11-11 15:58:07 +0700

    Add files via upload

---
+++ Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EatHealthyWPF
+{
+    public partial class KalkulatorKesehatan : Window
+    {
+        public KalkulatorKesehatan()
+        {
+            InitializeComponent();
+        }
+
+        private void HitungButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            double beratBadan = double.Parse(BeratBadanTextBox.Text);
+            double tinggiBadan = double.Parse(TinggiBadanTextBox.Text);
+
+            double hasil = HitungIndeksMassaTubuh(beratBadan, tinggiBadan);
+            MessageBox.Show($"Indeks Massa Tubuh Anda adalah: {hasil}");
+        }
+
+        private double HitungIndeksMassaTubuh(double beratBadan, double tinggiBadan)
+        {
+
+            return beratBadan / (tinggiBadan * tinggiBadan);
+        } 
+    }
+}
+
---
+++ Window3.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EatHealthyWPF
+{
+    public partial class PelacakanCairan : Window
+    {
+        public PelacakanCairan()
+        {
+            InitializeComponent();
+        }
+
+        private void CatatAsupanCairan_Click(object sender, RoutedEventArgs e)
+        {
+            // Ambil nilai dari TextBox jumlah air minum
+            string jumlahAirMinum = TxtJumlahAirMinum.Text;
+
+            // Ambil nilai dari ComboBox jenis minuman
+            ComboBoxItem selectedJenisMinuman = (ComboBoxItem)CmbJenisMinuman.SelectedItem;
+            string jenisMinuman = selectedJenisMinuman.Content.ToString();
+
+            // Lakukan logika pencatatan asupan cairan di sini
+            MessageBox.Show($"Catatan Asupan Cairan:\nJumlah: {jumlahAirMinum} ml\nJenis: {jenisMinuman}");
+        }
+    }
+}
